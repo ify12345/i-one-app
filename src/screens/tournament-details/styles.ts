3d87484d358f99dcf195f390741a7610cc6187db
Nwakodo Ifeanyi <46559372+ify12345@users.noreply.github.com>
2025-03-16 23:56:44 +0100

    update

--- src/screens/tournament-details/styles.ts
+++ src/screens/tournament-details/styles.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable import/extensions */
 import { StyleSheet } from 'react-native';
 import { Colors } from '@/src/config/colors';
